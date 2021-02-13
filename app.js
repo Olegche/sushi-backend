50e25752dea76fac3f775bb5b58713c2e98dadd6
Olegche <64771084+Olegche@users.noreply.github.com>
2021-02-13 16:30:02 +0200

    make it better

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
-app.use(cors())
 //-----
 var app = express();
 const {port, databaseName}=require('./config')
