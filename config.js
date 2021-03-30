0a0a4fb0a28de664858c5cef1c0d93b3ef5bbc41
Olegche <64771084+Olegche@users.noreply.github.com>
2021-03-30 11:43:18 +0300

    env

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   mongoURI: `${ process.env.MONGODB_URL}${ process.env.DATABASE_NAME}`,
   port: process.env.PORT,
   //
-  SENGRID_API_KEY: SENGRID_API_KEY,
-  EMAIL_FROM: EMAIL_FROM,
-  BASE_URL: BASE_URL
+  SENGRID_API_KEY: process.env.SENGRID_API_KEY,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  BASE_URL: process.env.BASE_URL
   
 };
